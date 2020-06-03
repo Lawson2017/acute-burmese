2a404e1169071eadb4252a33d94d2b32a43b0e81
james lawson <jlawson079@gmail.com>
2020-06-02 17:32:38 -0700

    added some template filters and set up the DOM for multiple pages, navbar and mapped to data base

--- src/pages/views.py
+++ src/pages/views.py
@@ -2,9 +2,24 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def home_view(*args, **kwargs): # arguments and keyword arguments
-	return HttpResponse("<h1>Hello World!</h1>")
+def home_view(request, *args, **kwargs): # arguments and keyword arguments
+	print(args, kwargs)
+	print(request.user)
+	# return HttpResponse("<h1>Hello World!</h1>")
+	return render(request, "home.html", {})
+
+def contact_view(request, *args, **kwargs):
+	return render(request, "contact.html", {})	
 
 
-def contact_view(*args, **kwargs):
-	return HttpResponse("<h1>Contact Page</h1>")	
+def about_view(request, *args, **kwargs):
+	my_context = {
+		"title": "This is about us",
+		"this_is_ture": True,
+		"my_number": 123,
+		"my_list": [123, 4231, 312, 'abc'],
+		"my_html": "<h1>Hello World</h1>"
+
+	}
+
+	return render(request, "about.html", my_context)	
